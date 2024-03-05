b3e5d2bbb9bd57c35307cc1bd20a3e89acc60194
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-03-05 16:01:07 +0100

    wip profil

--- app/src/pages/profile.vue
+++ app/src/pages/profile.vue
@@ -1,5 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const color = ref('white')
+</script>
 
 <template>
-  <h1>Profile page</h1>
+  <div class="mt-4">
+    <h1>Mon Profil</h1>
+
+    <v-card
+      :color="color"
+      class="bg-white mt-10"
+      max-width="344"
+    >
+      <v-card-item>
+        <div>
+          <div class="text-h6 mb-1">
+            <v-img
+              class="w-50 rounded-"
+              alt="Avatar"
+              src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+            ></v-img>
+          </div>
+          <div class="text-caption">Greyhound divisely hello coldly fonwderfully</div>
+        </div>
+      </v-card-item>
+
+      <v-card-actions>
+        <v-btn>
+          Button
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+
 </template>
+
