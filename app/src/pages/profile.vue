f5b03dd4657f8b12fe6e5efc0da1755372fdd5da
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-03-05 21:45:03 +0100

    finalisation de la partie profil

--- app/src/pages/profile.vue
+++ app/src/pages/profile.vue
@@ -1,6 +1,23 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 const color = ref('white')
+import WebService from "@/services/WebService";
+
+const jsonData = ref(null); // Declare jsonData as a reactive variable
+
+// Function to fetch JSON data from the API
+async function fetchData() {
+  try {
+    const response = await WebService.get<any>("users"); // Await the response directly
+    jsonData.value = response; // Update jsonData with the response
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    // Handle errors here
+  }
+}
+
+fetchData();
+
 </script>
 
 <template>
@@ -10,27 +27,94 @@ const color = ref('white')
     <v-card
       :color="color"
       class="bg-white mt-10"
-      max-width="344"
     >
       <v-card-item>
         <div>
           <div class="text-h6 mb-1">
             <v-img
-              class="w-50 rounded-"
+              class="mx-auto rounded-circle "
+              style="width: 90px; border-radius: 9px;"
               alt="Avatar"
               src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
             ></v-img>
           </div>
-          <div class="text-caption">Greyhound divisely hello coldly fonwderfully</div>
-        </div>
+       </div>
       </v-card-item>
+      <v-form v-if="jsonData">
+        <v-container>
+          <v-row>
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <v-text-field
+                label="Nom"
+                :model-value="jsonData.lastName"
+                variant="solo"
+              ></v-text-field>
+            </v-col>
 
-      <v-card-actions>
-        <v-btn>
-          Button
-        </v-btn>
-      </v-card-actions>
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <v-text-field
+                label="Prénom"
+                :model-value="jsonData.firstName"
+                variant="outlined"
+              ></v-text-field>
+            </v-col>
+
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <v-text-field
+                label="Email"
+                :model-value="jsonData.email"
+                variant="outlined"
+              ></v-text-field>
+            </v-col>
+
+            <v-col
+              cols="12"
+              sm="6">
+              <v-file-input
+                accept="image/png, image/jpeg, image/bmp"
+                label="Photo Profil"
+                placeholder="Pick an avatar"
+                prepend-icon="mdi-camera"
+              >
+              </v-file-input>
+            </v-col>
+
+          </v-row>
+        </v-container>
+        <div class="d-flex justify-center ga-6 mb-8">
+          <v-btn
+            class="mt-4"
+            color="#7649FF"
+            @click="validate"
+          >
+            Enregistrer
+          </v-btn>
+
+          <v-btn
+            class="mt-4"
+            color="error"
+            @click="reset"
+          >
+            Annuler
+          </v-btn>
+        </div>
+      </v-form>
     </v-card>
+
   </div>
 
 </template>
+<style>
+.input_file{
+  width: 50px !important;
+}
+</style>
