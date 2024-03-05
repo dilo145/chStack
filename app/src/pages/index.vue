93de941e7a17eae7239f01b967587b35a55cce4c
Omar <oelhadidi@myges.fr>
2024-03-05 13:49:10 +0100

    Modif 05/03 12:30

--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <h1>home page</h1>
+  <h1>Home Page</h1>
 </template>
+
+<script setup>
+  const variants = ['elevated', 'flat', 'tonal', 'outlined']
+</script>
